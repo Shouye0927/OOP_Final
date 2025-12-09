5631be88b1c348ff10ab7188ed215db8b8eb1d77
chenjia0510 <168859822+chenjia0510@users.noreply.github.com>
2025-12-09 16:43:42 +0800

    dino-version env

--- part3/oop_project_env.py
+++ part3/oop_project_env.py
@@ -1,120 +1,95 @@
 '''
-Custom Gym environment
-https://gymnasium.farama.org/tutorials/gymnasium_basics/environment_creation/
+Gym Wrapper for Dino Fighter Ultimate
+檔案名稱: oop_project_env.py
 '''
 import gymnasium as gym
 from gymnasium import spaces
 from gymnasium.envs.registration import register
-from gymnasium.utils.env_checker import check_env
-
-import warehouse_robot  as wr
+import dino as wr # 匯入你的遊戲檔案
 import numpy as np
 
-# Register this module as a gym environment. Once registered, the id is usable in gym.make().
+# 註冊環境 ID
 register(
-    id='warehouse-robot-v0',                                # call it whatever you want
-    entry_point='oop_project_env:WarehouseRobotEnv', # module_name:class_name
+    id='dino-fighter-v0',
+    entry_point='oop_project_env:DinoEnv', # 這裡一定要對應下面的 class 名稱
 )
 
-# Implement our own gym env, must inherit from gym.Env
-# https://gymnasium.farama.org/api/env/
-class WarehouseRobotEnv(gym.Env):
-    # metadata is a required attribute
-    # render_modes in our environment is either None or 'human'.
-    # render_fps is not used in our env, but we are require to declare a non-zero value.
-    metadata = {"render_modes": ["human"], 'render_fps': 4}
+class DinoEnv(gym.Env):
+    metadata = {"render_modes": ["human"], 'render_fps': 30}
 
-    def __init__(self, grid_rows=4, grid_cols=5, render_mode=None):
-
-        self.grid_rows=grid_rows
-        self.grid_cols=grid_cols
+    def __init__(self, render_mode=None):
         self.render_mode = render_mode
+        self.game = wr.DinoGame()
+        
+        # Action: 0=RUN, 1=JUMP, 2=SHOOT, 3=DROP
+        self.action_space = spaces.Discrete(4)
 
-        # Initialize the WarehouseRobot problem
-        self.warehouse_robot = wr.WarehouseRobot(grid_rows=grid_rows, grid_cols=grid_cols, fps=self.metadata['render_fps'])
-
-        # Gym requires defining the action space. The action space is robot's set of possible actions.
-        # Training code can call action_space.sample() to randomly select an action. 
-        self.action_space = spaces.Discrete(len(wr.RobotAction))
-
-        # Gym requires defining the observation space. The observation space consists of the robot's and target's set of possible positions.
-        # The observation space is used to validate the observation returned by reset() and step().
-        # Use a 1D vector: [robot_row_pos, robot_col_pos, target_row_pos, target_col_pos]
+        # Observation: [Dino Y, Dino HP, Ammo, Nearest Obstacle Dist, Nearest Obstacle Type]
         self.observation_space = spaces.Box(
-            low=0,
-            high=np.array([self.grid_rows-1, self.grid_cols-1, self.grid_rows-1, self.grid_cols-1]),
-            shape=(4,),
-            dtype=np.int32
+            low=np.array([0, 0, 0, 0, 0]), 
+            high=np.array([1000, 3, 3, 1000, 5]), 
+            dtype=np.float32
         )
 
-    # Gym required function (and parameters) to reset the environment
     def reset(self, seed=None, options=None):
-        super().reset(seed=seed) # gym requires this call to control randomness and reproduce scenarios.
+        super().reset(seed=seed)
+        self.game.reset(seed=seed)
+        self.game.state = 'RUNNING' # AI 模式下直接開始
+        obs = self._get_obs()
+        if self.render_mode == 'human':
+            self.game.render()
+        return obs, {}
 
-        # Reset the WarehouseRobot. Optionally, pass in seed control randomness and reproduce scenarios.
-        self.warehouse_robot.reset(seed=seed)
-
-        # Construct the observation state:
-        # [robot_row_pos, robot_col_pos, target_row_pos, target_col_pos]
-        obs = np.concatenate((self.warehouse_robot.robot_pos, self.warehouse_robot.target_pos))
-        
-        # Additional info to return. For debugging or whatever.
-        info = {}
-
-        # Render environment
-        if(self.render_mode=='human'):
-            self.render()
-
-        # Return observation and info
-        return obs, info
-
-    # Gym required function (and parameters) to perform an action
     def step(self, action):
-        # Perform action
-        target_reached = self.warehouse_robot.perform_action(wr.RobotAction(action))
+        # 將 int 轉為 Enum
+        game_action = list(wr.Action)[action]
+        _, is_done = self.game.step(game_action)
 
-        # Determine reward and termination
-        reward=0
-        terminated=False
-        if target_reached:
-            reward=1
-            terminated=True
+        # 獎勵設計 (給 AI 參考用)
+        reward = 1
+        if is_done: reward = -100
 
-        # Construct the observation state: 
-        # [robot_row_pos, robot_col_pos, target_row_pos, target_col_pos]
-        obs = np.concatenate((self.warehouse_robot.robot_pos, self.warehouse_robot.target_pos))
+        obs = self._get_obs()
+        if self.render_mode == 'human':
+            self.game.render()
 
-        # Additional info to return. For debugging or whatever.
-        info = {}
+        return obs, reward, is_done, False, {}
 
-        # Render environment
-        if(self.render_mode=='human'):
-            print(wr.RobotAction(action))
-            self.render()
+    def _get_obs(self):
+        # 找出最近的一個障礙物
+        dist = 999
+        obs_type = 0 # 0=None, 1=Cactus, 2=Bird, 3=Bat, 4=Heal
+        
+        if self.game.obstacles:
+            # 找到第一個在恐龍右邊的障礙物
+            for obj in self.game.obstacles:
+                if obj.x > self.game.dino.x:
+                    dist = obj.x - self.game.dino.x
+                    if isinstance(obj, wr.Cactus): obs_type = 1
+                    elif isinstance(obj, wr.Bird): obs_type = 2
+                    elif isinstance(obj, wr.Bat): obs_type = 3
+                    elif isinstance(obj, wr.HealthPack): obs_type = 4
+                    break
+        
+        return np.array([
+            self.game.dino.y,
+            self.game.dino.hp,
+            3 - len(self.game.bullets),
+            dist,
+            obs_type
+        ], dtype=np.float32)
 
-        # Return observation, reward, terminated, truncated (not used), info
-        return obs, reward, terminated, False, info
-
-    # Gym required function to render environment
     def render(self):
-        self.warehouse_robot.render()
+        self.game.render()
 
-# For unit testing
+# AI 測試區 (Random Agent)
 if __name__=="__main__":
-    env = gym.make('warehouse-robot-v0', render_mode='human')
-
-    # Use this to check our custom environment
-    # print("Check environment begin")
-    # check_env(env.unwrapped)
-    # print("Check environment end")
-
-    # Reset environment
+    env = gym.make('dino-fighter-v0', render_mode='human')
     obs = env.reset()[0]
-
-    # Take some random actions
-    while(True):
-        rand_action = env.action_space.sample()
-        obs, reward, terminated, _, _ = env.step(rand_action)
-
-        if(terminated):
-            obs = env.reset()[0]
+    
+    print("AI Running Random Agent...")
+    for _ in range(500):
+        action = env.action_space.sample()
+        obs, reward, terminated, _, _ = env.step(action)
+        if terminated:
+            env.reset()
